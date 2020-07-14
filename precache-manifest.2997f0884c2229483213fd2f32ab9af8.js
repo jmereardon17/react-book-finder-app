cb21287c04ec09f242828f013bc39809e8417945
Jamie Reardon <jamie.reardon91@gmail.com>
2020-07-14 01:26:31 +0100

    Updates

---
+++ precache-manifest.2997f0884c2229483213fd2f32ab9af8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e094d37976e72340bf35ce42699dffe",
+    "url": "/react-book-finder-app/index.html"
+  },
+  {
+    "revision": "b97d0952ac4abdeeb46c",
+    "url": "/react-book-finder-app/static/css/main.0eada5a1.chunk.css"
+  },
+  {
+    "revision": "4a1b0e7dc19451c3f638",
+    "url": "/react-book-finder-app/static/js/2.3376d3ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-book-finder-app/static/js/2.3376d3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97d0952ac4abdeeb46c",
+    "url": "/react-book-finder-app/static/js/main.bf4c900a.chunk.js"
+  },
+  {
+    "revision": "4629ecb688691e61df3e",
+    "url": "/react-book-finder-app/static/js/runtime-main.1a177e76.js"
+  },
+  {
+    "revision": "b6754f747dab446ce53f3d9a495d0b53",
+    "url": "/react-book-finder-app/static/media/glasses-outline.b6754f74.svg"
+  }
+]);
